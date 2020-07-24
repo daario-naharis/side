7d5ed667f9c8043529613447bf3313a5c4c4e7c9
daario-naharis <arpitbaranwal410@gmail.com>
2020-07-24 06:15:56 +0530

    Next commit

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ import './App.css';
 import HackerEarth from 'hackerearth-node';
 
 const hackerEarth=new HackerEarth('','');
-//I removed the secret API key for security reasons 
+//I removed the secret API key for security reasons :
 //use your own API key as first parameter in HackerEarth()
 //for example const hackerEarth=new HackerEarth('SECRET_API_KEY','');
 
